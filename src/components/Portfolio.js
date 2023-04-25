b3ad6d1dcf4b5965a2350a87a31525724b0cc08e
Corbin <corbin.johns.cj@gmail.com>
2023-04-24 22:17:47 -0400

    fixed portfolio CSS and added images

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,53 +5,53 @@ const Portfolio = () => {
     const projects = [
         {
             title: 'Animal Shelter',
-            imgSrc: '../images/',
-            deployLink: 'https://example.com',
-            githubLink: 'https://example.com',
+            imgSrc: '/images/animalshelter.jpg',
+            deployLink: 'https://project2-animal-shelter.herokuapp.com/',
+            githubLink: 'https://github.com/marywebby/animal-shelter',
         },
         {
             title: 'Movie Mash',
-            imgSrc: '../images/',
-            deployLink: 'https://example.com',
-            githubLink: 'https://example.com',
+            imgSrc: '/images/moviemash.jpg',
+            deployLink: 'https://scicluna.github.io/movie-mash/',
+            githubLink: 'https://github.com/scicluna/movie-mash',
         },
         {
             title: 'JavaScript Quiz',
-            imgSrc: '../images/',
-            deployLink: 'https://example.com',
-            githubLink: 'https://example.com',
+            imgSrc: '/images/jsquiz.jpg',
+            deployLink: 'https://github.com/cxrstings/javascript-quiz',
+            githubLink: 'https://cxrstings.github.io/javascript-quiz/',
         },
         {
             title: 'Weather Dashboard',
-            imgSrc: '../images/',
-            deployLink: 'https://example.com',
-            githubLink: 'https://example.com',
+            imgSrc: '/images/weather.jpg',
+            deployLink: 'https://cxrstings.github.io/weather-forecast/',
+            githubLink: 'https://github.com/cxrstings/weather-forecast',
         },
         {
             title: 'My Previous Portfolio',
-            imgSrc: '../images/',
-            deployLink: 'https://example.com',
-            githubLink: 'https://example.com',
+            imgSrc: '/images/portfolio.jpg',
+            deployLink: 'https://cxrstings.github.io/myportfolio/',
+            githubLink: 'https://github.com/cxrstings/myportfolio',
         },
         {
             title: 'Password Generator',
-            imgSrc: '../images/',
-            deployLink: 'https://example.com',
-            githubLink: 'https://example.com',
+            imgSrc: '/images/passgen.jpg',
+            deployLink: 'https://cxrstings.github.io/javascriptPasswordGenerator/',
+            githubLink: 'https://github.com/cxrstings/javascriptPasswordGenerator',
         },
     ];
 
     return (
         <div className={styles.portfolio}>
             {projects.map((project, index) => (
-                <div key={index} className='project'>
+                <div key={index} className={styles.project}>
                     <h2>{project.title}</h2>
                     <a href={project.deployLink} target="_blank" rel="noopener noreferrer">
                         <img src={project.imgSrc} alt={project.title} />
                     </a>
-                    <div className="links">
-                        <a href={project.deployLink} target="_blank" rel="noopener noreferrer">Deployed App</a>
-                        <a href={project.githubLink} target="_blank" rel="noopener noreferrer">Github Repo</a>
+                    <div className={styles.links}>
+                        <a className={styles.button} href={project.deployLink} target="_blank" rel="noopener noreferrer">Deployed App</a>
+                        <a className={styles.button} href={project.githubLink} target="_blank" rel="noopener noreferrer">Github Repo</a>
                     </div>
                 </div>
             ))}
